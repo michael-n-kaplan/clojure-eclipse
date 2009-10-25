e25192816f27e4241f2e6c639e292f8728523b14
matthias.koester <matthias.koester@cf014f0e-abaa-11de-a87c-5360b77f26dc>
2009-10-25 15:39:28 +0000

    First version where source parser is actually called.

--- trunk/org.anachronos.clojure.core/src/org/anachronos/clojure/core/parser/ClojureSourceElementParser.java
+++ trunk/org.anachronos.clojure.core/src/org/anachronos/clojure/core/parser/ClojureSourceElementParser.java
@@ -1,22 +1,12 @@
 package org.anachronos.clojure.core.parser;
 
-import org.eclipse.dltk.compiler.ISourceElementRequestor;
-import org.eclipse.dltk.compiler.env.ISourceModule;
-import org.eclipse.dltk.compiler.problem.IProblemReporter;
-import org.eclipse.dltk.core.ISourceElementParser;
-import org.eclipse.dltk.core.ISourceModuleInfoCache.ISourceModuleInfo;
+import org.anachronos.clojure.core.ClojureNature;
+import org.eclipse.dltk.core.AbstractSourceElementParser;
 
-public class ClojureSourceElementParser implements ISourceElementParser {
+public class ClojureSourceElementParser extends AbstractSourceElementParser {
 
     @Override
-    public void parseSourceModule(ISourceModule module, ISourceModuleInfo mifo) {
-    }
-
-    @Override
-    public void setReporter(IProblemReporter reporter) {
-    }
-
-    @Override
-    public void setRequestor(ISourceElementRequestor requestor) {
+    protected String getNatureId() {
+	return ClojureNature.CLOJURE_NATURE;
     }
 }
