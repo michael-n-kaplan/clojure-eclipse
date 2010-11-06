41471de466da7daa1b1fbb19ec2322d0ffea12f0
matthias.koester <matthias.koester@cf014f0e-abaa-11de-a87c-5360b77f26dc>
2010-11-06 13:24:12 +0000

    Removed uncommented code.

--- org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/contentassist/ClojureReferenceProposalCreator.java
+++ org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/contentassist/ClojureReferenceProposalCreator.java
@@ -1,34 +1,6 @@
 package org.maschinenstuermer.clojure.ui.contentassist;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.xtext.common.types.TypesPackage;
 import org.eclipse.xtext.common.types.xtext.ui.TypeAwareReferenceProposalCreator;
-import org.eclipse.xtext.resource.IEObjectDescription;
-import org.eclipse.xtext.scoping.IScope;
-import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
-
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 
 public class ClojureReferenceProposalCreator extends TypeAwareReferenceProposalCreator {
-
-//	public void lookupCrossReference(EObject model, EReference reference, ICompletionProposalAcceptor acceptor,
-//			Predicate<IEObjectDescription> filter, Function<IEObjectDescription, ICompletionProposal> proposalFactory) {
-//		if (TypesPackage.Literals.JVM_TYPE.isSuperTypeOf(getEReferenceType(model, reference))) {
-//			super.lookupCrossReference(model, reference, acceptor, filter, proposalFactory);
-//		} else if (model != null) {
-//			IScope scope = getScopeProvider().getScope(model, reference);
-//			Iterable<IEObjectDescription> candidates = scope.getContents();
-//			for (IEObjectDescription candidate: candidates) {
-//				if (!acceptor.canAcceptMoreProposals())
-//					return;
-//				if (filter.apply(candidate)) {
-//					acceptor.accept(proposalFactory.apply(candidate));
-//				}
-//			}
-//		}
-//	}
-
 }
