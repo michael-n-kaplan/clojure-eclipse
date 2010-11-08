c9e10f2e3a7e9f9929bf9857a997f5d4b71bb2c1
matthias.koester <matthias.koester@cf014f0e-abaa-11de-a87c-5360b77f26dc>
2010-11-08 07:45:58 +0000

    Improved import resolution, but still needs more work! Removed unused extension and references to it.

--- trunk/org.maschinenstuermer.clojure/src/org/maschinenstuermer/clojure/scoping/ClojureImportedNamespaceAwareLocalScopeProvider.java
+++ trunk/org.maschinenstuermer.clojure/src/org/maschinenstuermer/clojure/scoping/ClojureImportedNamespaceAwareLocalScopeProvider.java
@@ -8,9 +8,10 @@ import java.util.Set;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider;
-import org.maschinenstuermer.clojure.clojure.File;
 import org.maschinenstuermer.clojure.clojure.Import;
+import org.maschinenstuermer.clojure.clojure.InNs;
 import org.maschinenstuermer.clojure.clojure.Literal;
+import org.maschinenstuermer.clojure.clojure.ModuleImport;
 import org.maschinenstuermer.clojure.clojure.Ns;
 import org.maschinenstuermer.clojure.clojure.PackageImport;
 import org.maschinenstuermer.clojure.clojure.Reference;
@@ -20,21 +21,8 @@ import com.google.common.collect.Lists;
 
 public class ClojureImportedNamespaceAwareLocalScopeProvider extends
 		ImportedNamespaceAwareLocalScopeProvider {
-	private Set<ImportNormalizer> defaultImports; 
-
-	public ClojureImportedNamespaceAwareLocalScopeProvider() {
-		super();
-		final Set<ImportNormalizer> imports = new HashSet<ImportNormalizer>();
-		imports.add(createImportNormalizer("java.lang.*"));
-		imports.add(createImportNormalizer("clojure.core.*"));
-		defaultImports = Collections.unmodifiableSet(imports);
-	}
 	
-	private final ClojureSwitch<Set<ImportNormalizer>> importNormalizer = new ClojureSwitch<Set<ImportNormalizer>>() {
-		public Set<ImportNormalizer> caseFile(File _) {
-			return defaultImports;			
-		};
-		
+	private final ClojureSwitch<Set<ImportNormalizer>> importNormalizer = new ClojureSwitch<Set<ImportNormalizer>>() {		
 		public Set<ImportNormalizer> defaultCase(EObject _) {
 			return Collections.emptySet();
 		};
@@ -58,35 +46,68 @@ public class ClojureImportedNamespaceAwareLocalScopeProvider extends
 			return imports;
 		};
 		
-		
 		public Set<ImportNormalizer> caseNs(final Ns object) {
 			final HashSet<ImportNormalizer> imports = new HashSet<ImportNormalizer>();
+			addPackage("java.lang.*", imports);
+			final boolean addClojureCoreImport = object.getName() == null || 
+				!"clojure.core".equals(object.getName());
+			if (addClojureCoreImport)
+				addPackage("clojure.core.*", imports);
 			for (final Reference ref : object.getRefs()) {
 				for (final PackageImport packageImport : ref.getPackages()) {
 					addPackageImport(imports, packageImport);
 				}
+				for (final ModuleImport moduleImport : ref.getUses()) {
+					addModuleImport(imports, moduleImport);
+				}
 			}
 			return imports;
 		};
+
+		public Set<ImportNormalizer> caseInNs(InNs _) {
+			final HashSet<ImportNormalizer> imports = new HashSet<ImportNormalizer>();
+			addPackage("java.lang.*", imports);
+			return imports;
+		};
+		
+		private void addPackage(final String packageName, final Set<ImportNormalizer> imports) {
+			imports.add(createImportNormalizer(packageName));
+		}
+		
+		private void addPackageImport(final Set<ImportNormalizer> importNormalizers,
+				final PackageImport packageImport) {
+			String importedPackage = packageImport.getPackage();
+			if (importedPackage != null) {
+				importedPackage = importedPackage.concat(".");
+				if (packageImport.getClasses().isEmpty()) {
+					importNormalizers.add(createImportNormalizer(importedPackage.concat("*")));
+				} else {
+					for (final String importedClass : packageImport.getClasses()) {
+						importNormalizers.add(createImportNormalizer(importedPackage.concat(importedClass)));
+					}
+				}
+			}
+		}
+		
+		private void addModuleImport(final Set<ImportNormalizer> importNormalizers,
+				final ModuleImport moduleImport) {
+//			String importedPackage = moduleImport.getPackage();
+//			if (importedPackage != null) {
+//				importedPackage = importedPackage.concat(".");
+//				if (moduleImport.getFns().isEmpty()) {
+//					importNormalizers.add(createImportNormalizer(importedPackage.concat("*")));
+//				} else {
+//					for (final String importedClass : moduleImport.getFns()) {
+//						importNormalizers.add(createImportNormalizer(importedPackage.concat(importedClass)));
+//					}
+//				}
+//			}
+		}
+
 	};
 	
 	@Override
 	protected Set<ImportNormalizer> internalGetImportNormalizers(final EObject context) {
 		return importNormalizer.doSwitch(context);
-	}
-
-	private void addPackageImport(final Set<ImportNormalizer> importNormalizers,
-			final PackageImport packageImport) {
-		String importedPackage = packageImport.getPackage();
-		if (importedPackage != null) {
-			importedPackage = importedPackage.concat(".");
-			if (packageImport.getClasses().isEmpty()) {
-				importNormalizers.add(createImportNormalizer(importedPackage.concat("*")));
-			} else {
-				for (final String importedClass : packageImport.getClasses()) {
-					importNormalizers.add(createImportNormalizer(importedPackage.concat(importedClass)));
-				}
-			}
-		}
 	}
 }
