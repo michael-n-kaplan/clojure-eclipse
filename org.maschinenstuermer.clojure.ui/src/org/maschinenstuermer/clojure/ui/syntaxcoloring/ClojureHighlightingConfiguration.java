aa4dbd30d1533f47d6e0d9e0eb0d601a53e0f2b8
matthias.koester <matthias.koester@cf014f0e-abaa-11de-a87c-5360b77f26dc>
2010-11-08 19:33:29 +0000

    Added rainbow parentheses highlighting.

--- org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/syntaxcoloring/ClojureHighlightingConfiguration.java
+++ org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/syntaxcoloring/ClojureHighlightingConfiguration.java
@@ -13,7 +13,8 @@ public class ClojureHighlightingConfiguration extends
 	public static final String LITERAL_ID = "literal";
 	public static final String MACRO_CALL_ID = "macroCall";
 	public static final String SPECIAL_FORM_ID = "specialForm";
-
+	public static final String[] PAREN_IDS = new String[] { "paren1", "paren2", "paren3", "paren4", "paren5"};
+	
 	@Override
 	public void configure(final IHighlightingConfigurationAcceptor acceptor) {
 		acceptor.acceptDefaultHighlighting(FUNCTION_CALL_ID, "Function call", functionCallTextStyle());
@@ -24,9 +25,51 @@ public class ClojureHighlightingConfiguration extends
 		
 		acceptor.acceptDefaultHighlighting(LITERAL_ID, "Literal", stringTextStyle());
 		
+		acceptor.acceptDefaultHighlighting(PAREN_IDS[0], "Parenthese 1", paren1TextStyle());
+		acceptor.acceptDefaultHighlighting(PAREN_IDS[1], "Parenthese 2", paren2TextStyle());
+		acceptor.acceptDefaultHighlighting(PAREN_IDS[2], "Parenthese 3", paren3TextStyle());
+		acceptor.acceptDefaultHighlighting(PAREN_IDS[3], "Parenthese 4", paren4TextStyle());
+		acceptor.acceptDefaultHighlighting(PAREN_IDS[4], "Parenthese 5", paren5TextStyle());
+		
 		acceptor.acceptDefaultHighlighting(COMMENT_ID, "Comment", commentTextStyle());
 		acceptor.acceptDefaultHighlighting(DEFAULT_ID, "Default", defaultTextStyle());
 		acceptor.acceptDefaultHighlighting(INVALID_TOKEN_ID, "Invalid Symbol", errorTextStyle());
+	}
+
+	public TextStyle paren1TextStyle() {
+		final TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(127, 0, 0));
+		textStyle.setStyle(SWT.BOLD);
+		return textStyle;
+	}
+
+	public TextStyle paren2TextStyle() {
+		final TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(0, 127, 0));
+		textStyle.setStyle(SWT.BOLD);
+		return textStyle;
+	}
+
+	public TextStyle paren3TextStyle() {
+		final TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(0, 0, 127));
+		textStyle.setStyle(SWT.BOLD);
+		return textStyle;
+	}
+
+
+	public TextStyle paren4TextStyle() {
+		final TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(127, 127, 0));
+		textStyle.setStyle(SWT.BOLD);
+		return textStyle;
+	}
+
+	public TextStyle paren5TextStyle() {
+		final TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(0, 127, 127));
+		textStyle.setStyle(SWT.BOLD);
+		return textStyle;
 	}
 
 	@Override
--- org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/syntaxcoloring/ClojureSemanticHighlightingCalculator.java
+++ org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/syntaxcoloring/ClojureSemanticHighlightingCalculator.java
@@ -1,35 +1,87 @@
 package org.maschinenstuermer.clojure.ui.syntaxcoloring;
 
-import org.eclipse.emf.ecore.EObject;
+import java.util.Stack;
+import java.util.regex.Pattern;
+
 import org.eclipse.xtext.parsetree.AbstractNode;
+import org.eclipse.xtext.parsetree.CompositeNode;
 import org.eclipse.xtext.parsetree.LeafNode;
-import org.eclipse.xtext.parsetree.NodeUtil;
+import org.eclipse.xtext.parsetree.util.ParsetreeSwitch;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 import org.maschinenstuermer.clojure.clojure.Call;
 
-public class ClojureSemanticHighlightingCalculator implements
-ISemanticHighlightingCalculator {
+public class ClojureSemanticHighlightingCalculator 
+	implements ISemanticHighlightingCalculator {
 
-	@Override
-	public void provideHighlightingFor(final XtextResource resource, final IHighlightedPositionAcceptor acceptor) {
-		if (resource == null)
-			return;
+	private static class HighlightingSwitch extends ParsetreeSwitch<Boolean> 
+		implements ISemanticHighlightingCalculator {
 
-		final Iterable<AbstractNode> allNodes = NodeUtil.getAllContents(
-				resource.getParseResult().getRootNode());
-		for (final AbstractNode node : allNodes) {
-			final EObject element = node.getElement();
-			if (element instanceof Call) {
-				for (final LeafNode leafNode : node.getLeafNodes()) {
-					if ("fn".equals(leafNode.getFeature())) {
-						acceptor.addPosition(leafNode.getOffset(), leafNode.getLength(), 
-								ClojureHighlightingConfiguration.FUNCTION_CALL_ID);
-						break;
-					}
+		private static Pattern OPEN_PARENS = Pattern.compile("[\\({\\[]");
+		private static Pattern CLOSE_PARENS = Pattern.compile("[\\)}\\]]");
+		
+		private Stack<Integer> parenIdIndexStack = new Stack<Integer>();
+
+		private IHighlightedPositionAcceptor acceptor;
+
+		private boolean inCall;
+
+		@Override
+		public void provideHighlightingFor(final XtextResource resource,
+				final IHighlightedPositionAcceptor acceptor) {
+			if (resource == null)
+				return;
+
+			this.acceptor = acceptor;
+			final CompositeNode rootNode = resource.getParseResult().getRootNode();
+			doSwitch(rootNode);
+		}
+
+		@Override
+		public Boolean caseCompositeNode(final CompositeNode compositeNode) {
+			inCall = compositeNode.getElement() instanceof Call; 
+			for (final AbstractNode child : compositeNode.getChildren()) {
+				doSwitch(child);
+			}
+			return true;
+		}
+
+		@Override
+		public Boolean caseLeafNode(final LeafNode leafNode) {
+			if (OPEN_PARENS.matcher(leafNode.getText()).matches()) {
+				final int parenIdIndex;
+				if (parenIdIndexStack.isEmpty()) 
+					parenIdIndex = 0;
+				else
+					parenIdIndex = (parenIdIndexStack.peek() + 1) 
+					% ClojureHighlightingConfiguration.PAREN_IDS.length;
+				addParenPosition(leafNode, parenIdIndex);
+				parenIdIndexStack.push(parenIdIndex);
+			} else if (CLOSE_PARENS.matcher(leafNode.getText()).matches() 
+					&& !parenIdIndexStack.isEmpty()) {
+				final int parenIdIndex = parenIdIndexStack.pop();
+				addParenPosition(leafNode, parenIdIndex);
+			} else if (inCall) {
+				if ("fn".equals(leafNode.getFeature())) {
+					acceptor.addPosition(leafNode.getOffset(), leafNode.getLength(), 
+							ClojureHighlightingConfiguration.FUNCTION_CALL_ID);
 				}
 			}
+			return true;
 		}
+
+		private void addParenPosition(LeafNode object, final int parenIdIndex) {
+			acceptor.addPosition(object.getOffset(), object.getLength(), 
+					ClojureHighlightingConfiguration.PAREN_IDS[parenIdIndex]);
+		}
+	};
+
+	@Override
+	public void provideHighlightingFor(final XtextResource resource, 
+			final IHighlightedPositionAcceptor acceptor) {
+		new HighlightingSwitch().
+			provideHighlightingFor(resource, acceptor);
 	}
 }
+
