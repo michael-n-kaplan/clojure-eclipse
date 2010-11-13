076a4da1f25a202e943a94b6890cdfb6447eb122
matthias.koester <matthias.koester@cf014f0e-abaa-11de-a87c-5360b77f26dc>
2010-11-13 11:29:21 +0000

    Improved grammar so that it can parse more of clore.clj.

--- org.maschinenstuermer.clojure.tests/src/org/maschinenstuermer/clojure/parser/ClojureParserTest.java
+++ org.maschinenstuermer.clojure.tests/src/org/maschinenstuermer/clojure/parser/ClojureParserTest.java
@@ -4,6 +4,7 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.xtext.junit.AbstractXtextTests;
 import org.maschinenstuermer.clojure.ClojureStandaloneSetup;
 import org.maschinenstuermer.clojure.clojure.Def;
+import org.maschinenstuermer.clojure.clojure.Defn;
 import org.maschinenstuermer.clojure.clojure.File;
 import org.maschinenstuermer.clojure.clojure.Form;
 import org.maschinenstuermer.clojure.clojure.Literal;
@@ -16,10 +17,10 @@ public class ClojureParserTest extends AbstractXtextTests {
 		super.setUp();
 		with(ClojureStandaloneSetup.class);
 	}
-	
+
 	public void testStaticMember() throws Exception {
 		final File file = (File) getModel("(in-ns 'test) System/out");
-		
+
 		assertNotNull(file);
 		final EList<Namespace> namespaces = file.getNamespaces();
 		assertEquals(1, namespaces.size());
@@ -28,19 +29,19 @@ public class ClojureParserTest extends AbstractXtextTests {
 		assertEquals(1, exprs.size());
 		assertTrue(exprs.get(0) instanceof Literal);
 		final Literal literal = (Literal) exprs.get(0);
-		
+
 		assertNotNull(literal.getType());
 		assertEquals("java.lang.System", literal.getType().getCanonicalName());
-		
+
 		assertNotNull(literal.getMember());
 		assertEquals("java.lang.System.out", literal.getMember().getCanonicalName());
 	}
-	
+
 	public void testImport() throws Exception {
 		final File file= (File) 
-			getModel("(import '(java.math BigDecimal))" 
-					+ "(def z BigDecimal/ZERO)");
-		
+		getModel("(import '(java.math BigDecimal))" 
+				+ "(def z BigDecimal/ZERO)");
+
 		final EList<Form> exprs = file.getExprs();
 		assertEquals(2, exprs.size());
 		assertTrue(exprs.get(1) instanceof Def);
@@ -51,12 +52,12 @@ public class ClojureParserTest extends AbstractXtextTests {
 		assertEquals("java.math.BigDecimal", literal.getType().getCanonicalName());
 		assertEquals("java.math.BigDecimal.ZERO", literal.getMember().getCanonicalName());
 	}
-	
+
 	public void testNsWithImport() throws Exception {
 		final File file= (File) 
-			getModel("(ns test (:import (java.math BigDecimal)))" 
-					+ "(def z BigDecimal/ZERO)");
-		
+		getModel("(ns test (:import (java.math BigDecimal)))" 
+				+ "(def z BigDecimal/ZERO)");
+
 		final EList<Namespace> namespaces = file.getNamespaces();
 		assertEquals(1, namespaces.size());
 		final Namespace ns = namespaces.get(0);
@@ -71,4 +72,21 @@ public class ClojureParserTest extends AbstractXtextTests {
 		assertEquals("java.math.BigDecimal.ZERO", literal.getMember().getCanonicalName());
 	}
 
+	public void testDefn_SpecialCases() throws Exception {
+		File file= (File) 
+		getModel("(defn / [] \"div\") (/ 12 1)");
+
+		assertEquals(2, file.getExprs().size());
+		assertTrue(file.getExprs().get(0) instanceof Defn);
+		Defn defn = (Defn) file.getExprs().get(0);
+		assertEquals("/", defn.getName());
+
+		file= (File) 
+			getModel("(defn .. [] 1)");
+
+		assertEquals(1, file.getExprs().size());
+		assertTrue(file.getExprs().get(0) instanceof Defn);
+		defn = (Defn) file.getExprs().get(0);
+		assertEquals("..", defn.getName());
+	}
 }
