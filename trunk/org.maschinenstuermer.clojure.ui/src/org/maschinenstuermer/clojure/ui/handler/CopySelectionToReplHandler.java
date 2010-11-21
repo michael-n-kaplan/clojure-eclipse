dab463198e2e7009b0befa2aba02c0765ae3f80f
matthias.koester <matthias.koester@cf014f0e-abaa-11de-a87c-5360b77f26dc>
2010-11-21 15:37:00 +0000

    Added copy selection command, refactored abstract handler.

--- trunk/org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/handler/CopySelectionToReplHandler.java
+++ trunk/org.maschinenstuermer.clojure.ui/src/org/maschinenstuermer/clojure/ui/handler/CopySelectionToReplHandler.java
@@ -1,15 +1,17 @@
 package org.maschinenstuermer.clojure.ui.handler;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class EvaluateSelectionInReplHandler extends AbstractCopyTextToReplHandler {
+public class CopySelectionToReplHandler extends AbstractCopyTextToReplHandler {
 
-	public String getTextToCopy(final ExecutionEvent event) {
+	@Override
+	public String getTextToCopy(ExecutionEvent event) {
 		final ISelection selection = HandlerUtil.getCurrentSelection(event);
 		final ITextSelection textSelection = (ITextSelection) selection;
-		final String selectedText = textSelection.getText().concat("\n");
-		return selectedText;
+		final String text = textSelection.getText();
+		return text;
 	}
 }
